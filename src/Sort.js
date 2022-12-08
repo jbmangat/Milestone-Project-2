1029d9e77a15eb282f2b05fae15e69ec4e0df6dd
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-12-07 23:15:07 -0800

    jesus added sort and addworkout components

---
+++ src/Sort.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Sort = () => {
+  return (
+    <div>
+        <div>
+            <button className='sort-btn-name'>Sort By Name</button>
+        </div>
+        <div>
+            <button className='sort-btn-type'>Sort By Type</button>
+        </div>
+        <div>
+            <button className='sort-btn-date'>Sort By Date</button>
+        </div>
+    </div>
+  )
+}
+
